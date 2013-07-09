9518d18818eebfeb53eafe6b35b603ed781217aa
Gabor Heja <gheja@cubeone>
2013-07-09 00:06:44 +0000

    Added engine/core/consts.h

---
+++ src/engine/core/consts.h
@@ -0,0 +1,14 @@
+#ifndef ENGINE_CORE_CONSTS_H
+#define ENGINE_CORE_CONSTS_H
+
+const DIRECTION_UP = 0;
+const DIRECTION_RIGHT = 1;
+const DIRECTION_DOWN = 2;
+const DIRECTION_LEFT = 3;
+
+const SIDE_TOP = 0;
+const SIDE_RIGHT = 1;
+const SIDE_BOTTOM = 2;
+const SIDE_LEFT = 3;
+
+#endif
